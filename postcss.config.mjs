2e8559ea9c4e86ce5931f2bfd2dd43cfb45da0ee
andrewleevey-ux <andrew.leevey@gmail.com>
2026-01-29 22:24:51 +0000

    Initial UI build for FlashCopy

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
-const config = {
+export default {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {}, // Update this line
+    autoprefixer: {},
   },
 };
-
-export default config;
